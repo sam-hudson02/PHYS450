c90b58d5a9e27a017647fda623f114b91edd2d2d
Sam Hudson <samhudson@outlook.com>
2025-10-28 16:24:31 +0000

    added plotting to edge.py

--- src/edge.py
+++ src/edge.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.constants import hbar as hbar_SI, e as eC
 import matplotlib.pyplot as plt
-from alive_progress import alive_bar
 
 class EdgeStates:
     def __init__(self, n: int, hop: np.ndarray,
@@ -65,11 +64,44 @@ class EdgeStates:
             q (np.ndarray): A 2D momentum vector.
             mag (np.ndarray): A 2D magnetic field vector.
         Returns:
-            np.ndarray: |psi|^2 for each layer.
+            np.ndarray: |psi|^2 for the zero energy state.
         """
         ham = self.hamiltonian(q, mag)
-        _, evecs = np.linalg.eigh(ham)
-        psi_sq = np.abs(evecs) ** 2
-        print(psi_sq.shape)
-        print(psi_sq)
+        evals, evecs = np.linalg.eigh(ham)
+        zero_state = None
+        for i, val in enumerate(evals):
+            if np.isclose(val, 0, atol=1e-9):
+                zero_state = evecs[:, i]
+                break
+        if zero_state is None:
+            raise ValueError("No zero energy state found.")
+        psi_sq = np.abs(zero_state)**2
         return psi_sq
+
+    def plot_edge_state(self, psi_sq: np.ndarray, mag: np.ndarray):
+        """
+        Plot the edge state probability density as bar graph.
+        Args:
+            psi_sq (np.ndarray): The probability density |psi|^2.
+        """ 
+        j_max = 2 * self.n + 1
+        j = np.arange(1, j_max) # atomic site
+        plt.figure(figsize=(8, 6))
+        plt.xlabel('j')
+        plt.ylabel(r'$|\psi|^2$')
+        plt.title(f'Edge State Probability Density\nMagnetic Field: Bx={mag[0]} T, By={mag[1]} T')
+        plt.bar(j, psi_sq, width=0.8, color='blue', alpha=0.7, label=r'$|\psi|^2$')
+        plt.xticks(ticks=np.arange(0, j_max, 20))
+        plt.legend()
+        plt.savefig(f'./plots/edge_state_Bx{mag[0]}_N{self.n}.png')
+
+    def run(self, q: np.ndarray, mag: np.ndarray):
+        """
+        Run the edge state calculation and plot the result.
+        Args:
+            q (np.ndarray): A 2D momentum vector.
+            mag (np.ndarray): A 2D magnetic field vector.
+        """
+        psi_sq = self.solve_hamiltonian(q, mag)
+        self.plot_edge_state(psi_sq, mag)
+
